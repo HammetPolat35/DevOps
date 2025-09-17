84165952318177ded2fb903c87cfb436cfe2228d
HammetPolat35 <hammetpolat@gmail.com>
2025-09-17 21:07:57 +0300

    first commit

---
+++ build/cpp_pubsub/CMakeFiles/test_my_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_my_node.dir/test/test_my_node.cpp.o"
+  "test_my_node"
+  "test_my_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_my_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/cpp_pubsub/colcon_command_prefix_test.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/bmcotonom1/DevOps (second copy )/install/cpp_pubsub/share/cpp_pubsub/package.sh"
