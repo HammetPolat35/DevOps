487fe3e8d4c3c3957bcfade0430ed2167cdecadb
HammetPolat35 <hammetpolat@gmail.com>
2025-09-18 13:53:02 +0300

    test ci

--- src/cpp_pubsub/src/subscriber_member_function.cpp
+++ src/cpp_pubsub/src/subscriber_member_function.cpp
@@ -11,14 +11,14 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-This will fail
+thisWillFail
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 using std::placeholders::_1;
 
-int unused_int=40;
+int unused_var = 42;
 
 class MinimalSubscriber : public rclcpp::Node
 {
